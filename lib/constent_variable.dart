42c88d36de8e4d9ba5e68abace89b22a4cfc84ca
madiharafiq <madiharafiq929@gmail.com>
2020-11-25 20:42:35 +0500

    My App Calcultor

---
+++ lib/constent_variable.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const activationcolor = Colors.indigo;
+const deactivationcolor = Colors.black;
+
+const ConstentTextLabel = TextStyle(
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+);
+
+const ConstentTextLabe2 = TextStyle(
+  fontSize: 60,
+  fontWeight: FontWeight.w900,
+);
+
+const ConstentTextLabe3 = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w900,
+);
+
+const Result1 = TextStyle(
+  color: Colors.lightBlueAccent,
+  fontSize: 20.0,
+);
+const Result2 = TextStyle(
+  color: Colors.white,
+  fontSize: 80.0,
+  fontWeight: FontWeight.w900,
+);
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'MainPage.dart';
+
+
+void main() => runApp(SplashScreen());
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MySplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => BmiCalculatur())));
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/icons.png', height: 250.0, width: 250,),
+
+          SizedBox( height: 30.0),
+
+          SpinKitRipple(color: Colors.black, size: 150.0, borderWidth: 20.0,),
+        ],
+      ),
+    );
+  }
+}
